ed733891d07995cc2232b318aac1b2f0fe6dc68c
Славик Лис <Fooxboy@yandex.ru>
2019-01-10 13:37:19 +0300

    Добавлен проект "Война миров"

---
+++ Fooxboy.WarOfTheWordGame/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text;
+
+namespace Fooxboy.WarOfTheWordGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.UTF8;
+            Console.Title = "Война миров";
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
