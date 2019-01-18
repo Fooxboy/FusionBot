28f22124772d6f42eccc34f7f11e9159acbda5ca
Славик Лис <Fooxboy@yandex.ru>
2019-01-18 19:42:43 +0300

    back up

---
+++ Fooxboy.WarOfTheWordGame/Databases/Battle/Now.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fooxboy.WarOfTheWordGame.Databases.Battle
+{
+    public class Now  
+    {
+
+
+
+        public long[] Users { get; set; }
+        public long BattleId { get; set; }
+        public Coordinates[] Coordinates { get; set; }
+        public long TimeStart { get; set; }
+        public long TimeEnd { get; set; }
+    }
+
+    public class Coordinates
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+}
